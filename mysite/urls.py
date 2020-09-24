21af8b467524a3c964668e34a48d70e2c40cd8e1
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2020-09-24 18:43:05 +0200

    feat: swagger docs

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,21 +14,50 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from rest_framework import routers
 from polls import views
 
-API_URL = "api"
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = routers.DefaultRouter()
-router.register(API_URL + r"/users", views.UserViewSet)
-router.register(API_URL + r"/groups", views.GroupViewSet)
-router.register(API_URL + r"/questions", views.QuestionViewSet)
-router.register(API_URL + r"/choices", views.ChoiceViewSet)
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
+router.register(r"questions", views.QuestionViewSet)
+router.register(r"choices", views.ChoiceViewSet)
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Polls Adminstration",
+        default_version="v1",
+        description="Questions and Choices",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="gulfaraz@gulfaraz.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path("", include(router.urls)),
+    path("api/", include(router.urls)),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
